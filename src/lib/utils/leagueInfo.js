92d3658e9c887d63ce384955440d56bfa7302106
ittwa <123038339+ittwa@users.noreply.github.com>
2023-08-06 19:27:04 -0400

    Update leagueInfo.js
    breezy <3s lil'jordan humphrey

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -224,7 +224,7 @@ export const managers = [
         link: 9, // manager array number within this array, or null to link back to all managers page
         image: "/managers/question.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
-      "favoritePlayer": 5096, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+      "favoritePlayer": 5938, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Draft Pats players", // (optional)
