f928b85cfd6db6f90885cd650c7bcde3c9575b8c
ittwa <123038339+ittwa@users.noreply.github.com>
2023-08-07 22:29:22 -0400

    Update leagueInfo.js
    changed Jorge's name

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -79,7 +79,7 @@ export const managers = [
       {
       // "roster": 3,  // (DEPRECATED! Don't use this anymore) ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID": "389115841381158912",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
-      "name": "Albarran",
+      "name": "Jorge",
       // "tookOver": 2014, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Boston", // (optional)
       "bio": "my name is jeff",
