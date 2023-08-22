430a1ddb656e7b40855dcc43a6458ca43ae27bda
ittwa <123038339+ittwa@users.noreply.github.com>
2023-08-22 09:19:06 -0400

    Update +page.svelte
    through article 3

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -208,113 +208,62 @@
 
 
     <hr />
-    
+
+
     <h2 class="sectionHeading" bind:this={one}>Article 1 Membership</h2>
+
     
     <h3 class="subSectionHeading" bind:this={oneOne}>1.1 League Size</h3>
     
-    <p>28 Total Players</p>
-    
-    <p class="underscore">Starters</p>
-    <ul>
-        <li>QB</li>
-        <li>RB</li>
-        <li>RB</li>
-        <li>WR</li>
-        <li>WR</li>
-        <li>TE</li>
-        <li>FLEX (RB/WR/TE)</li>
-        <li>FLEX (RB/WR)</li>
-        <li>D/ST</li>
-        <li>K</li>
-    </ul>
-    
-    <p>18 Bench (Expanded by 5 between the keague draft and start of regular season)</p>
-
-    <p>1 IR Spots (Player must be labeled as IR, Out, or Covid in the Sleeper App)</p>
-    
-        <h4 bind:this={oneOneOne}>1.1.1 Position Maximums</h4>
-        
-        <div class="subBlock">
-            <table class="positionMaximums">
-                <tr><td>QB</td><td class="right">3 active, 5 total</td></tr>
-                <tr><td>RB</td><td class="right">20</td></tr>
-                <tr><td>WR</td><td class="right">20</td></tr>
-                <tr><td>TE</td><td class="right">10</td></tr>
-                <tr><td>D/ST</td><td class="right">3</td></tr>
-                <tr><td>K</td><td class="right">3</td></tr>
-            </table>
-        </div>
+    <p>The composition of membership for the League shall be twelve (12) Owners.</p>
 
 
-    <h3 class="subSectionHeading" bind:this={oneTwo}>1.2 Trading</h3>
+    <h3 class="subSectionHeading" bind:this={oneTwo}>1.2 Annual Dues</h3>
     
-    <p>Trading of players and draft picks (up to 3 drafts away) are allowed. Trades will process immediately and will be reversed if vetoed or under investigation for collusion. Tradable assets include players, draft picks, and FAAB $.</p>
+    <p>The current annual cost of ownership is $150.</p>
     
-    <h4 bind:this={oneTwoOne}>1.2.1 Trade Collusion</h4>
-    <div class="subBlock">
-        <p>If any owners are suspected of accepting bribes/offering bribes to enhance their/another owners team via trade, trading any assets other than fantasy players, draft picks, FAAB $, or otherwise found guilty of engaging in any form of collusion*, all parties involved in the trade will be susceptible to punishment. The punishments will go as followed:</p>
-        
-        <ul>
-            <li>First Offense – Immediate reduction of FAAB budget by 80%</li>
-            <li>Second Offense – Immediate freeze on all transaction types for the next 18 weeks of regular season and playoff fantasy football games, including trades and waiver wire bids</li>
-            <li>Third Offense – A league meeting of all members not involved in any of the improper trades/activity in question will take place to discuss next steps.</li>
-        </ul>
+    
+    <h3 bind:this={oneThree}>1.3 Payment Schedule</h3>
+    
+    <p>Payment of the membership fee must be turned in to the Commish (cash or Venmo) by the date of the Free Agent Auction.</p>
 
-        <p>All trades that are found guilty of collusion, will be reversed/denied. If the trade had affected the outcome of previous games, the results of those games will be revised.  </p>
 
-        <p>*Collusion includes the organizing of veto votes against an acceptable trade.</p>
+    <h3 bind:this={oneFour}>1.4 League Platform</h3>
+    
+    <p>The League will use <a href="https://sleeper.com/leagues/918205115247595520/matchup">Sleeper</a> as the platform.</p>
 
-        <p>The commissioner will err on the side of letting owners manage their teams the way they see fit. Any collusion will need to be clear. The commissioner reserves the right to step in and at the very least assess the thinking of any team involved in a heavily lop-sided trade. </p>
-    </div>
 
-    <h4 bind:this={oneTwoTwo}>1.2.2 Lending Players Prohibition</h4>
-    <div class="subBlock">
-        <p>Any player traded from a team may not be re-acquired via trade within 6 weeks of the original trade. Any teams involved in this behavior will be subject to the aforementioned collusion punishments.</p>
-    </div>
     
-    <h4 bind:this={oneTwoThree}>1.2.3 Trade Deadline</h4>
-    <div class="subBlock">
-        <p>The trade deadline will be set for the Saturday of the week 11 games of the NFL season.</p>
-    </div>
+    <h2 class="sectionHeading" bind:this={two}>Article 2 Ownership</h2>
     
-    <h4 bind:this={oneTwoFour}>1.2.4 Trade Restrictions</h4>
-    <div class="subBlock">
-        <p>You will not be able to trade draft picks that are more than 3 drafts away. For example, during the 2021 season you cannot trade any picks in or after the 2025 draft. You may only trade FAAB $ from the current season.</p>
-    </div>
-    
-    <h4 bind:this={oneTwoFive}>1.2.5 Veto/Trade Process</h4>
-    <div class="subBlock">
-        <p>A total of 7 veto votes are required to veto a trade. Vetoes are only appropriate in instances of collusion or an egregious mismatch of value (i.e. CMC for a backup kicker). Not liking a trade does not warrant a veto.</p>
-    </div>
-    
-    
-    
-    <h3 bind:this={oneThree}>1.3 Waiver Wire</h3>
-    
-    <p>FAAB Waiver wire system. Each team will begin the season with $100. $0 bids are allowed. Bids will process Thursdays and Sundays at 9AM EST.</p>
+    <h3 bind:this={twoOne}>2.1 Ownership of Team</h3>
+    <p>Ownership of a membership lies in the hands of the League.  A member may not sell his rights of Ownership to anyone.  If a member wishes to leave the League, he shall report his decision to the Commissioner who will in-turn alert the other members of the League and begin to contact individuals on the League’s Waiting List in search of a replacement.</p>
 
-    <p>FAAB budgets will reset each season in January.</p>
+    <h3 bind:this={twoTwo}>2.2 Waiting List</h3>
+    <p>The New Owner will be selected from the <a href="https://docs.google.com/spreadsheets/d/17kspYjtSNtiBuUxbdWYkHbP8Y5K7qs0nJ-D1VA-Wpwo/edit?pli=1#gid=372338541">Waiting List</a> in the order by which they joined. The potential New Owner who has been on the Waiting List the longest will have the first shot to be voted in by the current Owners.</p>
     
-    
-    <h2 class="sectionHeading" bind:this={two}>Section 2 Drafting</h2>
-    
-    <h3 bind:this={twoOne}>2.1 Draft Order</h3>
-    <p>The draft order each year will be a linear draft in reverse order of the previous year’s regular season standings.</p>
+    <h3 bind:this={twoThree}>2.3 New Owners</h3>
+    <p>New Owners must be voted in by the rest of the League (majority vote) in order to avoid douchebags and uncommitted owners.  The new member must have a Sponsor that recommends them to the League.</p>
 
-    <h3 bind:this={twoTwo}>2.2 Rookie Drafts</h3>
-    <p>Rokkie drafts will consist of 3 round of drafting, with 5 minutes alotted for each pick. To make room for the incoming players, benches will be expanded by (5) spots. These spots will be removed in mid-August, at which point you will need to have cut the necessary players.</p>
-    <p>Any spots not filled during the draft, can be filled immediately afterwards from the waiver wire.</p>
-    
-    <h3 bind:this={twoThree}>2.3 Draft Day Trades</h3>
-    <p>If a trade is being made during the draft, one or both parties can alert the commissioner to pause the draft to allow for extra time to negotiate the trade.</p>
-    
-    <h2 class="sectionHeading" bind:this={three}>Section 3 Scoring System</h2>
+    <h3 bind:this={twoFour}>2.4 Co-Ownership</h3>
+    <p>Teams may be co-owned.</p>
 
-    <h3 bind:this={threeOne}>3.1 Scoring System</h3>
-    <p>The league scoring system breakdown is listed on the Sleeper fantasy app (for the most part, it follows typical .5PPR Scoring). The scoring system will never change without a majority league vote.</p>
     
-    <h2 class="sectionHeading" bind:this={four}>Section 4 Postseason</h2>
+    <h2 class="sectionHeading" bind:this={three}>Article 3 Governance</h2>
+
+    <h3 bind:this={threeOne}>3.1 Code of Conduct</h3>
+    <p>All active members of the League are expected to conduct themselves in a manner consistent with that of a Gentleman.  This code of ethics includes, but is not limited to: only ripping fellow Owners as much as they can handle; being respectful of and sensitive to Liam’s (mental, phyiscal, and mental) disabilities; not dropping players out of anger, frustration, or resentment (or some stupid, personal NFL boycott or other such nonsense); not partaking in collusion of any sort.</p>
+
+    <h3 bind:this={threeTwo}>3.2 Voting</h3>
+    <p>This League is run as a democracy. All matters of significance will be determined by a vote. If it is a vote on a proposed new rule, that rule will pass with a majority (>50%) vote. If it is a vote to overturn an existing rule then a ⅔ quorum is needed to pass the vote. Each team receives a single vote. If a team is co-owned, only one vote from that co-ownership will be counted.
+</p>
+
+    <h3 bind:this={threeThree}>3.3 Removal Procedures</h3>
+    <p>If there are grounds (complaints from several Owners, inactivity, lack of communication, repeated violations to the Code of Conduct, etc.) to remove an Owner from the League the Commish will first discuss the potential removal with the Owner. If the Commish deems there is a reasonable case for removal of the Owner, the Commish and the Owner will present the case to the rest of the League. If a solution cannot be reached without a vote, a vote for removal will be had. A ⅔ quorum is needed to pass the vote to remove the Owner from the League.</p>
+
+
+
+    <h2 class="sectionHeading" bind:this={four}>Article 4 Postseason</h2>
     
     <h3 bind:this={fourOne}>4.1 Playoffs</h3>
     <p>At the end of the season 6 teams will make the playoffs. The first round of playoffs will commence in week 15. Each team that wins their division and the 4 wild card teams that finish with the best records, will receive a playoff berth. The 2 teams that win their division will receive a bye week in the first round of the playoffs. Each playoff matchup will only last one week and the winner will advance. Week 18 will not be used.</p>
@@ -331,12 +280,12 @@
         <li>Duel to the death</li>
     </ol>
 
-    <h2 class="sectionHeading" bind:this={five}>Section 5 Tanking Policy</h2>
+    <h2 class="sectionHeading" bind:this={five}>Article 5 Tanking Policy</h2>
     
     <h3 bind:this={fiveOne}>5.1 League Tanking Policy</h3>
     <p>No team may intentionally leave starting roster spots empty. Any evidence of intentionally leaving roster spots empty in order to improve your draft position may be subject to punishment, including but not limited to the forfeiture of draft selections.</p>
 
-    <h2 class="sectionHeading" bind:this={six}>Section 6 Replacing Managers</h2>
+    <h2 class="sectionHeading" bind:this={six}>Article 6 Replacing Managers</h2>
     
     <h3 bind:this={sixOne}>6.1 Removing Managers</h3>
     <p>Short of continuously failing to set a valid lineup, or confirmed collusion, no manager can be removed against their will. Participation, through the league chat, trade offers, and the waiver wire, is strongly encouraged but generally grounds for removal.</p>
@@ -347,7 +296,7 @@
     <h3 bind:this={sixThree}>6.3 Replacement Incentive</h3>
     <p>In the event that a manager chooses to quit the league or is removed by a comissioner, an incoming manager is offered a 50% reductioon on their first year buy-in.</p>
 
-    <h2 class="sectionHeading" bind:this={seven}>Section 7 League Finances</h2>
+    <h2 class="sectionHeading" bind:this={seven}>Article 7 League Finances</h2>
     
     <h3 bind:this={sevenOne}>7.1 League Dues</h3>
     <p>League dues are set at {dues}$. Dues are collected through <a href="https://www.leaguesafe.com/league/3949641">LeagueSafe</a> and collection will be sent out no later than the first of August. All managers must be fully paid up before the start of regular season. In the event that a manager fails to pay before the deadline, a 10% penalty will be addd to their payment and will go towards the payouts at the end of the year.</p>
