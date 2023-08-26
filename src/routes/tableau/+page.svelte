d1be6e76601e962c7ee4ea0e2e1c1890bcdc12cc
ittwa <123038339+ittwa@users.noreply.github.com>
2023-08-25 22:07:34 -0400

    Create +page.svelte
    for tableau/

---
+++ src/routes/tableau/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  // You can import any necessary modules or variables here
+</script>
+
+<style>
+  .main {
+    position: relative;
+    z-index: 1;
+    display: block;
+    margin: 30px auto;
+    width: 95%;
+    max-width: 1000px;
+    overflow-y: hidden;
+  }
+</style>
+
+<div class="main">
+  <!-- Embedded iframe -->
+  <iframe src="https://public.tableau.com/views/IThoughtThisWasAmericaFFL/Rosters?" frameborder="0"></iframe>
+</div>
