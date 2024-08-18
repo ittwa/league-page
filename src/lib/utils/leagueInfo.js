05e6da06a63a3ef5d8a63f7711275e9913bebd90
ittwa <123038339+ittwa@users.noreply.github.com>
2024-08-18 09:58:08 -0400

    Update leagueInfo.js
    updated league ID

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
-export const leagueID = "918205115247595520"; // your league ID
+export const leagueID = "1048335951803654144"; // your league ID
 export const leagueName = "I Thought This Was America"; // your league name
-export const dues = 150; // (optional) used in template constitution page
+export const dues = 200; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
